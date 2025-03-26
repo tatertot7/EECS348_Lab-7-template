40ead5afba3612937889e51f8c3d79d034dcfbd7
Tate <tateg04@ku.edu>
2025-03-26 16:02:49 -0500

    written main program to take user input for a score, then call print combinations/count combinations from football.c if input is invalid, program will terminate.

--- football_main.c
+++ football_main.c
@@ -1,7 +1,24 @@
 #include "football.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main() {
     int points;
+
+    //loop to continuously prompt user
+    while (1) { //while true
+        printf("Enter the NFL score: ");
+        scanf("%d", &points);
+
+        // if invalid number (0 or 1)
+        if (points <= 1) {
+            prinf("Invalid Score.\n");
+            break;
+        }
+        //call print_comb from football.c
+        print_combinations(points);
+        //call count_comb and print from football.c
+        printf("Total combinations: %d\n", count_combinations(points));
+    }
+    
     return 0;
 }
